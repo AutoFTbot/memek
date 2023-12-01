8da5ac693f3f68fcf000344199133c19c3a36a82
AutoFTbot <151357338+AutoFTbot@users.noreply.github.com>
2023-12-02 02:18:50 +0700

    Add files via upload

---
+++ file/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "AutoFtGroup Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script By AutoFtGroup" | lolcat
+echo "                   Author Script AutoFtBot" | lolcat
+echo "-----------------------------------------------------"
